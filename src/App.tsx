a60ada1430de01bc931b1228618440775d42447a
Matthew Hess <matthew9510@gmail.com>
2023-01-06 17:22:40 -0500

    Updated App.tsx to use InterviewWorkspace and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+import InterviewWorkspace from './Components/InterviewWorkspace';
 
 function App() {
   return (
+    <div className='flex flex-col justify-between h-screen'>
     <Navbar/>
+    <InterviewWorkspace/>
+    <Footer/>
+    </div>
   );
 }
 
