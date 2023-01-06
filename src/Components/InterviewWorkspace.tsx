25e59bbc18d54ea51698824514082aca0262501e
Matthew Hess <matthew9510@gmail.com>
2023-01-06 17:20:44 -0500

    Interview Workspace area created

---
+++ src/Components/InterviewWorkspace.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import {Wrapper} from '../Components/styles'
+
+interface IProps {
+  interviewerName?: string;
+}
+
+const HelloWalmart: React.FC<IProps> = (props: IProps) => {
+  return (
+    <div className="h-full flex justify-center  justify-self-center item-center border-[#FCB61A] border-[15px]">
+      <h1 className="self-center text-[24px] text-[#0071dc] font-bold">{props.interviewerName}'s Workspace!</h1>
+    </div>
+  );
+};
+
+HelloWalmart.defaultProps = {
+    interviewerName: "Matthew",
+};
+
+export default HelloWalmart;
---
+++ src/Components/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.section`
+display: flex;
+justify-content: center;
+align-content: center
+`
