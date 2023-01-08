1305521a23a5b30c06a83820008b32301fc77272
Matthew Hess <matthew9510@gmail.com>
2023-01-07 21:08:23 -0500

    Adjusted font-size of 🪄 in footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ import { AiOutlineLink, AiOutlineCopyrightCircle } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="bg-[#004f9a] h-28 font-bold flex flex-col items-center justify-center text-center text-white">
-      <p className="text-sm sm:text-base md:text-base mt-3 pb-2">This Product was made by Matthew Hess using 🪄</p> 
+      <p className="text-sm sm:text-base md:text-base mt-3 pb-2">This Product was made by Matthew Hess using <span className="text-lg sm:text-xl md:text-2xl">🪄</span></p> 
       <p className="mb-2 text-[10px] sm:text-sm">Inspired by <span className="underline"><a href="https://github.com/andreibarbu5/walmart">Andrei Barbu's Walmart Replica Project</a></span> <AiOutlineLink className="inline"/>, and is by no means necessary affiliated with <AiOutlineCopyrightCircle className="inline"/> 2023 Walmart</p>
     </div>
   );
