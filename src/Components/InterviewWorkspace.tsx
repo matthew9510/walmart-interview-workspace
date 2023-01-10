2da9abfcf1e0d398271c87644e388d58bb490908
Matthew Hess <matthew9510@gmail.com>
2023-01-09 21:55:55 -0800

    Altered InterviewWorkspace element name

--- src/Components/InterviewWorkspace.tsx
+++ src/Components/InterviewWorkspace.tsx
@@ -5,7 +5,7 @@ interface IProps {
   interviewerName?: string;
 }
 
-const HelloWalmart: React.FC<IProps> = (props: IProps) => {
+const InterviewWorkspace: React.FC<IProps> = (props: IProps) => {
   return (
     <div className="h-full flex justify-center  justify-self-center item-center border-[#FCB61A] border-[15px]">
       <h1 className="self-center text-[24px] text-[#0071dc] font-bold">{props.interviewerName}'s Workspace!</h1>
@@ -13,8 +13,8 @@ const HelloWalmart: React.FC<IProps> = (props: IProps) => {
   );
 };
 
-HelloWalmart.defaultProps = {
+InterviewWorkspace.defaultProps = {
     interviewerName: "Matthew",
 };
 
-export default HelloWalmart;
+export default InterviewWorkspace;
